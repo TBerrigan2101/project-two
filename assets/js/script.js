e2b43e4f3a431f221bfad21e32cbc624d2f447e9
TBerrigan2101 <tberrigan2101@gmail.com>
2022-11-03 23:54:52 +0000

    Add functionality to game js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const computerImage = document.getElementById("computer-image");
 const choices = ["square", "circle", "star", "plus-sign", "wavy-lines"]
 
 
+
 for (let button of buttons) {
     button.addEventListener("click", function() {
        let playerChoice = this.getAttribute("data-type");
@@ -16,9 +17,35 @@ function playGame (playerChoice) {
     playerImage.src = `assets/images/${choices[playerChoice]}.png`;
     playerImage.alt = choices[playerChoice];
 
-    computerChoice = Math.floor(Math.random() * 5);
+    let computerChoice = Math.floor(Math.random() * 5);
 
     computerImage.src = `assets/images/${choices[computerChoice]}.png`;
     computerImage.alt = choices[computerChoice];
 
+    let result = checkResult(choices[playerChoice], choices[computerChoice]);
+
 }
+
+function checkResult (playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        increasePlayerScore();
+    } else {
+        increaseComputerScore();
+    }
+}
+
+
+function increasePlayerScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
+}
+
+function increaseComputerScore() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
+}
+
