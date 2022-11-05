58eddc5d558baefe967979a93cc3c40f802a459a
TBerrigan2101 <tberrigan2101@gmail.com>
2022-11-05 22:20:40 +0000

    Added reset timer to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const choices = ["square", "circle", "star", "plus-sign", "wavy-lines"]
 const correctReply = ["Incredible", "Amazing", "You can’t see these can you?", "You are a legitimate phenomenon", "You’re no fluke"]
-const wrongReply = ["Good guess, but wrong", "Oooh close. But definitely wrong.", "Sorry, this isn’t your lucky day", "Do you even want the five bucks?", "Do I have to shock you again?"]
+const wrongReply = ["Good guess, but wrong.", "Oooh close. But definitely wrong.", "Sorry, this isn’t your lucky day", "Do you want the five bucks?", "Do I have to shock you again?"]
+const intervalText = ["Clear your head", "Nervous?", "What is it?", "Come on!", "Think hard. What is it?"]
 
 
 
@@ -25,6 +26,9 @@ function playGame (playerChoice) {
     computerImage.alt = choices[computerChoice];
 
     let result = checkResult(choices[playerChoice], choices[computerChoice]);
+
+    setTimeout(resetGame, 3000);
+        
 
 }
 
@@ -60,5 +64,14 @@ function increaseComputerScore() {
 
 }
     
+function resetGame () {
 
+    playerImage.src = `assets/images/player-image.png`;
 
+    computerImage.src = `assets/images/computer-image.png`;
+
+    let intervalResponse = Math.floor(Math.random() * 5);
+    let nextGuess = intervalText[intervalResponse];
+    document.getElementById("computer_response").innerText = nextGuess;
+
+    }
